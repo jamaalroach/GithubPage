70119198010699bfb372fd345f9de7703a1d6905
ellocke <ilja_s@pm.me>
2018-05-28 14:35:25 +0200

    added (custom) Thumbnail to CSS & Template (summary.html)

--- 0_setup.R
+++ 0_setup.R
@@ -4,8 +4,6 @@
 ## blogdown::new_site(theme = "olOwOlo/hugo-theme-even")
 ### don't forget to change publishDir in config.toml
 
-
-
 # Some frequently used functions for Publishing with Blogdown/Hugo
 
 # esp. in case of seImpl / tomlparseImpl() errors
