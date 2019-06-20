1e2e7c0c119032827b741ae5585f375323f3a84d
fubits <ilja_s@pm.me>
2019-06-20 19:26:45 +0200

    progress bar

--- static/js/custom.js
+++ static/js/custom.js
@@ -60,8 +60,13 @@ function scrollFunction() {
       document.getElementsByClassName("menu")[0].style.marginTop = "";
       document.getElementsByClassName("menu")[0].style.marginBottom = ""
     }
-  } 
-}
+  }
 
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress-bar").style.width = scrolled + "%";
+
+}
 
 // <!-- custom Ende -->
